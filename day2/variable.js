7f51a89f217dbb70c8ec0f088f0bec54863baacf
irakozemarie <irakoze.antonny@gmail.com>
2024-02-26 13:16:39 +0200

    first commit

---
+++ day2/variable.js
@@ -0,0 +1,16 @@
+/*  variables and datatypes*/
+var age=12;//number as datatypes
+var my_name="sylivie";//string variable
+const array=["string",12,false];
+const myfavfood =["rice","beans","chips","cassava"];
+console.log(" my name is " +  my_name + " and am " + age + " years old");
+console.log('my name is ${my_name} and am ${age}years old');
+console.log(array);
+console.log(myfavfood);
+function myname()
+{
+    var mine="chance";
+console.log(mine);
+}
+myname();
+
